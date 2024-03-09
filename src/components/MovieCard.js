0566188170e0ec99b1949f7227d16e823107251b
LundPiyush <piyushlund349@gmail.com>
2024-03-09 16:24:40 +0530

    fix(): backdrop paths fix

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,7 +8,10 @@ const MovieCard = ({ posterPath, id, isLarge, backdropPath }) => {
   console.log(imgPath);
   return (
     <Link to={`/movie/${id}`}>
-      <div className="pr-4 w-36 md:w-48 transition-transform duration-450 hover:scale-105">
+      <div
+        className={`pr-4 transition-transform duration-450 hover:scale-105 ${
+          isLarge ? "w-36 md:w-48" : "w-48 md:w-60"
+        }  `}>
         <img
           className={isLarge ? "min-h-52" : "min-h-32"}
           src={IMG_CDN_URL + imgPath}
