4da4da0bb75f8964ccc1e18f135a217eadd6ed26
LundPiyush <piyushlund349@gmail.com>
2024-03-09 15:52:00 +0530

    fix(): fixed image size responsiveness

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 
 const MovieCard = ({ posterPath, id, isLarge, backdropPath }) => {
   if (!posterPath) return null;
-  const imgPath = IMG_CDN_URL + isLarge ? posterPath : backdropPath;
+  const imgPath = isLarge ? posterPath : backdropPath;
+  console.log(imgPath);
   return (
     <Link to={`/movie/${id}`}>
       <div className="pr-4 w-36 md:w-48 transition-transform duration-450 hover:scale-105">
         <img
-          src={imgPath}
+          src={IMG_CDN_URL + imgPath}
           //src="https://m.media-amazon.com/images/M/MV5BZWIyNzE3NzEtMGExNS00ZjRkLWJmMTYtMWFlNTNkNDgyNWUzXkEyXkFqcGdeQXVyODUwMzI5ODk@._V1_.jpg"
           alt="movie"
         />
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -8,7 +8,6 @@ export const isMovieInLikesList = (likesList, movieId) => {
 
 export const findRating = (ratingsList, id) => {
   const existingRating = ratingsList.find(({ movieId }) => movieId === id);
-
   // Assign initialValue based on whether the movie ID exists in the ratings list
   const ratingValue = existingRating ? existingRating.rating : 0;
   return ratingValue;
